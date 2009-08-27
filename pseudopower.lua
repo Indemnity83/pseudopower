93a6125e89f79bffa08f60e333ef8825a03e2d31
Indemnity83 <Indemnity83@537e489a-92b5-11de-a307-bbee0a36ac66>
2009-08-27 03:03:34 +0000

    Version 1.0.0 (Historical Commit)

---
+++ pseudopower.lua
@@ -0,0 +1,116 @@
+---------------
+-- Libraries --
+---------------
+local StatLogic = LibStub("LibStatLogic-1.1")
+local TipHooker = LibStub("LibTipHooker-1.1")
+
+
+---------------------
+-- Local variables --
+---------------------
+local DEBUG = true
+local quality_threshold = 2
+
+
+-----------------
+-- Debug Tools --
+-----------------
+local function debugPrint(text)
+	if DEBUG == true then
+		print(text)
+	end
+end
+
+
+-------------------
+-- Toptip Script --
+-------------------
+local function OnTooltipSetItem(self)
+	local _, Item = self:GetItem()
+	if Item then
+ 		local pp, pph = GetValue(Item)
+ 		
+ 		if pp then
+			if pph > pp then
+     			self:AddLine(L["PseudoPower: %d (%d)"]:format(pp, pph))		
+        	else 
+				self:AddLine(L["PseudoPower: %d"]:format(pp))
+			end
+		end		
+	end
+end
+
+-----------------------
+-- Add tooltip hooks --
+-----------------------
+local _, class = UnitClass("player")
+if (class == "PRIEST") then
+	-- GameTooltip:SetScript("OnTooltipSetItem", OnTooltipSetItem)
+	-- ItemRefTooltip:SetScript("OnTooltipSetItem", OnTooltipSetItem)	
+	TipHooker:Hook(OnTooltipSetItem, "item")
+end 
+
+
+-----------------
+-- Multiplyers --
+-----------------
+local SPELL_POWER = 1
+local SPELL_HIT = 1.53
+local SPELL_CRIT = 0.72
+local SPELL_HASTE = 0.68
+local BONUS_INT = 0.22
+local BONUS_SPI = 0.25
+
+
+------------------------------------------------------
+-- Used to display PP values for special case items --
+------------------------------------------------------
+local CUSTOM_ITEM_DATA = {
+	-- Equip Chance Items
+	[39229] = { 147.8, 147.8 },
+}
+
+
+--------------------------------
+-- Calculate PP and PPH value --
+--------------------------------
+function GetValue(item)
+	if not item then return end
+	
+	local _, itemLink, rarity, _, _, _, _, _, _ = GetItemInfo(item)
+	if not itemLink then return end
+	
+	-- Is the item above our minimum threshold?
+	if not rarity or rarity < quality_threshold then
+		return nil, nil
+	end
+	
+	-- Get the item ID to check against custom data
+	local itemID = itemLink:match("item:(%d+)")
+	if not itemID then return end
+	itemID = tonumber(itemID)
+
+	-- Check to see if there is custom data for this item ID
+	if CUSTOM_ITEM_DATA[itemID] then
+		pp, pph = unpack(CUSTOM_ITEM_DATA[itemID])
+		return pp, pph
+	end
+	
+	-- Build Summary Table
+	local statData = {}
+	StatLogic:GetSum(itemLink, statData)
+			
+	-- Do the math for base PsudoPower
+	local pp = 0
+	if (statData["SPELL_DMG"]) then pp = pp + statData["SPELL_DMG"] * SPELL_POWER end
+	if (statData["SPELL_CRIT_RATING"]) then pp = pp + statData["SPELL_CRIT_RATING"] * SPELL_CRIT end
+	if (statData["SPELL_HASTE_RATING"]) then pp = pp + statData["SPELL_HASTE_RATING"] * SPELL_HASTE end
+	if (statData["INT"]) then pp = pp + statData["INT"] * BONUS_INT end
+	if (statData["SPI"]) then pp = pp + statData["SPI"] * BONUS_SPI end
+	
+	-- Do the final calculation including Hit
+	local pph = pp
+	if (statData["SPELL_HIT_RATING"]) then pph = pph + statData["SPELL_HIT_RATING"] * SPELL_HIT end
+		
+	return pp, pph
+end	
